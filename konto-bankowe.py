13410d1aec40946de2f5f468ed9135995a525783
Tomasz Mioduszewski <340450@DC=sntis,DC=local>
2024-11-06 13:24:22 +0100

    Konto bankowe jeszcze bez logów

--- konto-bankowe.py
+++ konto-bankowe.py
@@ -2,13 +2,14 @@ import random
 import datetime
 
 money = random.randint(1000,10000)
-loan = int(0)
+loan = 0
 while 1>0:
     print("Wyświetl swój stan konta - wpisz 1")
     print("Przelej pieniądze na konto - wpisz 2")
     print("Przelej pieniądze z konta - wpisz 3")
     print("Weź pożyczkę - 4")
     print("Wyjście - 5")
+    
     number = int(input("Co chcesz zrobić?: "))
     
     if number == 1:
@@ -17,12 +18,14 @@ while 1>0:
         value1 = int(input(f"Podaj wartość przelewu. (Maksymalny przelew na konto to 20000PLN): "))
         if value1 <= 20000 and value1 > 0:
             money = money + value1
+            print(f"Stan konta po przelewie na konto: {money}ZŁ")
 
     elif number == 3:
         account = int(input(f"Podaj numer konta: "))
         value2 = int(input(f"Podaj wartość przelewu. (Maksymalny przelew z konta to 10000PLN)"))
         if value2 <= 10000 and value2 > 0:
-             money = money - value2
+            money = money - value2
+            print(f"Stan konta po przelewie na inne konto: {money} ZŁ")
 
     elif number == 4:
         credit = int(input(f"Podaj wartość pożyczki. (Maksymalna wysokość pożyczki to 5000PLN)"))
@@ -31,6 +34,7 @@ while 1>0:
         if credit <= 5000 and credit > 0 and loan == 0:
             money = money + credit
             loan = loan + 1 
+            print(f"Stan konta po pożyczce: {money} ZŁ")
         elif loan == 1: 
             print("Nie możesz wziąć więcej niż jedną pożyczkę")
 
